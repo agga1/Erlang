d273e81ebb3e95b80e7d9141de871c1dc77f5595
agga1 <agud100@gmail.com>
2020-03-11 16:31:20 +0100

    extended onp done

--- ErlFirst/src/onp.erl
+++ ErlFirst/src/onp.erl
@@ -17,6 +17,11 @@ onpIns([A, B |T], ["+" | T2]) -> onpIns([B+A | T], T2);
 onpIns([A, B |T], ["-" | T2]) -> onpIns([B-A | T], T2);
 onpIns([A, B |T], ["*" | T2]) -> onpIns([B*A | T], T2);
 onpIns([A, B |T], ["/" | T2]) -> onpIns([B/A | T], T2);
+onpIns([A, B |T], ["^" | T2]) -> onpIns([math:pow(B, A) | T], T2);
+onpIns([A |T], ["sqrt" | T2]) -> onpIns([math:sqrt(A) | T], T2);
+onpIns([A |T], ["sin" | T2]) -> onpIns([math:sin(A) | T], T2);
+onpIns([A |T], ["cos" | T2]) -> onpIns([math:cos(A) | T], T2);
+onpIns([A |T], ["tg" | T2]) -> onpIns([math:tan(A) | T], T2);
 onpIns(L, [Sth| T2]) -> onpIns([toNum(Sth)|L], T2).
 onp(Str) -> onpIns([], string:tokens(Str, " ")).
 
