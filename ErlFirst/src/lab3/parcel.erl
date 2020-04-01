1452ba9eadfc8a12ad49b9c14b22b0a9ea7aa314
agga1 <agud100@gmail.com>
2020-04-01 23:10:55 +0200

    semi parallel

--- ErlFirst/src/lab3/parcel.erl
+++ ErlFirst/src/lab3/parcel.erl
@@ -2,7 +2,7 @@
 -author("Agnieszka Dutka").
 
 %% API
--export([randList/1, findMyParcelLocker/2, findLockersSeq/2, findLoop/3, findLockersParallel/2]).
+-export([randList/1, findMyParcelLocker/2, findLockersSeq/2, findLoop/3, findLockersParallel/2, findSingle/3, findLockersSemiParallel/3]).
 
 %% Data
 randList(N) -> [ {random:uniform(10001)-1,random:uniform(10001)-1}  || _<-lists:seq(1, N)].
@@ -23,14 +23,33 @@ findLockersSeq(Persons, Lockers)->
 
 findLockersParallel(_, []) ->  {error, no_lockers};
 findLockersParallel(Persons, Lockers) ->
-  lists:foreach(fun(P)-> spawn(?MODULE, findLoop, [self(), P, Lockers]) end,Persons),
-  gatherResults([], length(Persons)).
+  lists:foreach(fun(P)-> spawn(?MODULE, findSingle, [self(), P, Lockers]) end,Persons),
+  gatherResult([], length(Persons)).
+
+gatherResult(Results, 0)-> Results;
+gatherResult(Results, Rest)->
+  receive
+    Res -> gatherResult([Res|Results], Rest-1)
+  end.
+
+findSingle(PID, Person, Lockers)->
+  PID ! {Person, findMyParcelLocker(Person, Lockers)}.
+
+findLockersSemiParallel(_, [], _) ->  {error, no_lockers};
+findLockersSemiParallel(Persons, Lockers, Cores) ->
+  LP = length(Persons),
+  NthStart = fun(I) -> (LP*(I-1) div Cores) + 1 end,
+  NthLen = fun(I) -> NthStart(I+1)- NthStart(I) end,
+  lists:foreach(fun(CoreNr)->
+    spawn(?MODULE, findLoop, [self(), lists:sublist(Persons, NthStart(CoreNr),NthLen(CoreNr) ), Lockers])
+                end, lists:seq(1, Cores)),
+  gatherResults([], Cores).
 
 gatherResults(Results, 0)-> Results;
 gatherResults(Results, Rest)->
   receive
-    Res -> gatherResults([Res|Results], Rest-1)
+    Res -> gatherResults(Res++Results, Rest-1)
   end.
 
-findLoop(PID, Person, Lockers)->
-  PID ! {Person, findMyParcelLocker(Person, Lockers)}.
+findLoop(PID, Persons, Lockers)->
+  PID ! findLockersSeq(Persons, Lockers).
