e7c771e0f6100495a06709b8e0a0c26cae1fb7c5
agga1 <agud100@gmail.com>
2020-04-06 21:34:32 +0200

    added comments, more tests, macros working

--- src/lab2/pollution.erl
+++ src/lab2/pollution.erl
@@ -1,18 +1,18 @@
 -module(pollution).
 -author("Agnieszka Dutka").
 -record(station, {coords, name, measurements =#{}}).
--record(mkey, {datetime, type}).
--define(IS_COORD(Geo), is_tuple(Geo), size(Geo)==2, is_number(element(1, Geo)), is_number(element(2, Geo))).
+-record(measkey, {datetime, type}).
 -define(IS_NAME(Name), is_list(Name)).
--define(IS_DATE(Date), is_tuple(Date), size(Date)==3, is_integer(element(1, Date)), is_integer(element(2, Date)), is_integer(element(3, Date))).
--define(IS_TIME(Time), is_tuple(Time), size(Time)==3, is_integer(element(1, Time)), is_integer(element(2, Time)), is_integer(element(3, Time))).
--define(IS_DATE_TIME(Datetime), is_tuple(Datetime), size(Datetime)==2, ?IS_DATE(element(1, Datetime)), ?IS_TIME(element(2, Datetime))).
+-define(IS_COORD(Geo), (is_tuple(Geo) andalso  size(Geo)==2 andalso  is_number(element(1, Geo)) andalso  is_number(element(2, Geo)))).
+-define(IS_DATE(Date), (is_tuple(Date) andalso size(Date)==3 andalso is_integer(element(1, Date)) andalso is_integer(element(2, Date)) andalso  is_integer(element(3, Date)))).
+-define(IS_TIME(Time), (is_tuple(Time) andalso size(Time)==3 andalso is_integer(element(1, Time)) andalso is_integer(element(2, Time)) andalso is_integer(element(3, Time)))).
+-define(IS_DATE_TIME(Datetime), (is_tuple(Datetime) andalso size(Datetime)==2 andalso ?IS_DATE(element(1, Datetime)) andalso ?IS_TIME(element(2, Datetime)))).
 %% API
-%%-export([createMonitor/0]).
 -export([createMonitor/0, addStation/3, addValue/5, removeValue/4, getOneValue/4, getStationMean/3, getDailyMean/3, getPeakHours/2, getHourlyMean/3, mostActiveStation/1]).
 %%
 createMonitor() -> [].
-%%
+%% ADDING STATION -----------------------------------------------------------------------
+%% arguments:    (Monitor, Station Name, Station Coordinates)   returns: Monitor with added station
 addStation(_, Name, _) when not ?IS_NAME(Name) -> {error, illegal_name};
 addStation(_, _, Coord) when not ?IS_COORD(Coord) ->{error, illegal_coordinates};
 addStation(Stations, Name, Coord)->
@@ -23,16 +23,17 @@ addStation(Stations, Name, Coord)->
   end.
 
 %% ADDING MEASUREMENT -----------------------------------------------------------------------
-% adding measurement to list of measurements Ms
-addMeasurement(_, Date, Type,_)  when not ?IS_DATE_TIME(Date);  not is_list(Type) -> throw("incorrect measurement data");
+% adding measurement to given list of measurements Ms
+addMeasurement(_, Date, Type,_)  when not ?IS_DATE_TIME(Date);  not is_list(Type) -> {error, incorrect_datatype};
 addMeasurement(Ms, Date, Type,Value) ->
-  Found = maps:is_key(#mkey{datetime = Date, type = Type}, Ms),
+  Found = maps:is_key(#measkey{datetime = Date, type = Type}, Ms),
   case Found of
     true -> {error, measurement_exists};
-    _ -> maps:put(#mkey{datetime = Date, type = Type}, Value, Ms)
+    _ -> maps:put(#measkey{datetime = Date, type = Type}, Value, Ms)
   end.
 % searching for station with given Id and modifying its measurements map
-%% arguments:    addValue(Monitor, Id,  Date, Type, Value)
+%% arguments:    addValue(Monitor, StationId (Name or Coordinates),  Date, Type, Value),
+%% returns:      modified Monitor (or tuple {error, error_message} )
 addValue(_, Id,  _, _, _) when not ?IS_NAME(Id) and not ?IS_COORD(Id) -> {error, incorrect_id};
 addValue( [], _, _, _, _) -> {error, station_not_found};
 addValue( [ St = #station{name= Name, measurements = Ms} | Stations ], Name, Date, Type, Value) ->
@@ -55,22 +56,23 @@ addValue( [ H | Stations ], Id, Date, Type, Value) ->
   end.
 
 %% REMOVING MEASUREMENT -----------------------------------------------------------------------
-% arguments: (Monitor, StationId, Date, Type)
-removeValue([], _, _, _) ->  throw("incorrect measurement data");
+% removes measurement with given Date and Type from station
+% arguments: (Monitor, StationId, Date, Type), returns: modified Monitor
+removeValue([], _, _, _) -> [];  % quiet remove
 removeValue([ St = #station{name= Name, measurements = Ms} | Stations ], Name, Date, Type) ->
-  [St#station{measurements = maps:remove(#mkey{datetime = Date, type = Type}, Ms)} | Stations ];
+  [St#station{measurements = maps:remove(#measkey{datetime = Date, type = Type}, Ms)} | Stations ];
 removeValue([ St = #station{coords = Coord, measurements = Ms} | Stations ], Coord, Date, Type) ->
-  [St#station{measurements = maps:remove(#mkey{datetime = Date, type = Type}, Ms)} | Stations ];
+  [St#station{measurements = maps:remove(#measkey{datetime = Date, type = Type}, Ms)} | Stations ];
 removeValue([H|Stations], Id, Date, Type) -> [H|removeValue(Stations, Id, Date, Type)].
 
 %% GET ONE VALUE ------------------------------------------------------------------------------
-% for fetching value of measurement with given Date and Type
+% returns:   value of measurement with given Date and Type
 % arguments: (Monitor, StationId, Date, Type)
 getOneValue([], _, _, _)  -> {error, station_not_found};
 getOneValue([#station{name= Name, measurements = Ms} | _ ], Name, Date, Type)  ->
-  maps:get(#mkey{datetime = Date, type = Type}, Ms);
+  maps:get(#measkey{datetime = Date, type = Type}, Ms);
 getOneValue([#station{coords = Coord, measurements = Ms} | _ ], Coord, Date, Type)  ->
-  maps:get(#mkey{datetime = Date, type = Type}, Ms);
+  maps:get(#measkey{datetime = Date, type = Type}, Ms);
 getOneValue([_|Stations], Id, Date, Type) -> getOneValue(Stations, Id, Date, Type).
 
 %% MEAN --------------------------------------------------------------------------------------
@@ -78,15 +80,15 @@ calculateMean(_, 0) -> 0.0;  % exclude division by 0
 calculateMean(Sum, N) -> Sum/N.
 
 %% STATION MEAN ------------------------------------------------------------------------------
-% returns mean value of all measurements of a given type for a given station
 % arguments: (Monitor, StationId, Type)
+% returns: mean value of all measurements of a given type for a given station
 getStationMean([], _, _) -> {error, station_not_found};
 getStationMean([#station{name= Name, measurements = Ms} | _ ], Name, Type) -> getMean(Ms, Type);
 getStationMean([#station{coords= Coord, measurements = Ms} | _ ], Coord, Type) -> getMean(Ms, Type);
 getStationMean([_ | Stations ], Id, Type) -> getStationMean(Stations, Id, Type).
 
 getMean(Ms, Type) ->
-  Filtered = maps:filter(fun(#mkey{type = T}, _) -> T==Type end, Ms),
+  Filtered = maps:filter(fun(#measkey{type = T}, _) -> T==Type end, Ms),
   Sum = maps:fold( fun(_, V, Acc) -> V+Acc end, 0, Filtered),
   calculateMean(Sum, maps:size(Filtered)).
 
@@ -95,14 +97,14 @@ getMean(Ms, Type) ->
 % arguments: (Monitor, Day, Type)
 getDailyMean(Stations, Day, Type) ->
   ToFilteredValues = fun(#station{measurements = Ms}, Day, Type) ->
-    maps:values( maps:filter(fun(#mkey{type = T, datetime = {FDay, _}}, _) -> T==Type andalso FDay==Day end, Ms) )
+    maps:values( maps:filter(fun(#measkey{type = T, datetime = {FDay, _}}, _) -> T==Type andalso FDay==Day end, Ms) )
     end,
   Values = lists:foldl(fun(St, Acc)-> ToFilteredValues(St, Day, Type)++Acc end ,[], Stations),
   calculateMean(lists:sum(Values), length(Values)).
 
 %% HOUR OF HIGHEST PEAK -------
 % find hours in which average value for given type of measurement across stations is the highest
-% returns {highest average for an hour , hours in which it occurred }
+% returns: tuple {highest average for an hour , hours in which it occurred }
 getPeakHours(Stations, Type) ->
   AvgForHour = [ {Hour, getHourlyMean(Stations, Hour, Type)} || Hour<-lists:seq(0, 23)],
   MaxVal = lists:foldl(fun({_, Val}, Acc)-> max(Val, Acc) end ,0,AvgForHour),
@@ -111,16 +113,17 @@ getPeakHours(Stations, Type) ->
   io:format("highest value: ~p~npeak hours: ~p~n", [MaxVal,Hours]),
   {MaxVal, Hours}.
 
+% get mean across stations for given hour of given measurement type
 getHourlyMean(Stations, Hour, Type) ->
   ToFilteredValues = fun(#station{measurements = Ms}, Hour, Type) ->
-    maps:values( maps:filter(fun(#mkey{type = T, datetime = {_, {FHour, _, _}}}, _) -> T==Type andalso FHour==Hour end, Ms) )
+    maps:values( maps:filter(fun(#measkey{type = T, datetime = {_, {FHour, _, _}}}, _) -> T==Type andalso FHour==Hour end, Ms) )
            end,
   Values = lists:foldl(fun(St, Acc)-> ToFilteredValues(St, Hour, Type)++Acc end ,[], Stations),
   calculateMean(lists:sum(Values), length(Values)).
 
 %% MOST ACTIVE STATION -------------------------------------------------------------------------
 % displays name of station(s) which registered the most measurements
-% returns Names of most active stations
+% returns: names of most active stations
 mostActiveStation(Stations) ->
   MsForEach = lists:map(fun(#station{name=Name, measurements = Ms})-> {Name, maps:size(Ms)} end , Stations),
   MaxMs = lists:foldl(fun({_, Val}, Acc)-> max(Val, Acc) end ,0,MsForEach),
--- src/lab2/pollution_tests.erl
+++ src/lab2/pollution_tests.erl
@@ -12,16 +12,20 @@ addStation_test() ->
   M = pollution:createMonitor(),
   M1 = pollution:addStation(M, "ONE", {1, 1}),
   M2 = pollution:addStation(M1,"TWO", {1, 2}),
-  ?assertEqual(2, length(M2)),
+  ?assertEqual(2, length(M2)),   % 2 stations should be added
   ?assertMatch({error, illegal_name}, pollution:addStation(M2, 3, {4, 1})),
   ?assertMatch({error, station_exists}, pollution:addStation(M2, "ONE", {2,3})),
-  ?assertMatch({error, station_exists}, pollution:addStation(M2, "THREE", {1, 1})).
+  ?assertMatch({error, station_exists}, pollution:addStation(M2, "THREE", {1, 1})),
+  ?assertMatch({error, illegal_coordinates}, pollution:addStation(M2, "THREE", {"2",3})),
+  ?assertMatch({error, illegal_coordinates}, pollution:addStation(M2, "THREE", {1, 2,3})).
 
 addValue_test()->
   M = pollution:createMonitor(),
   M1 = pollution:addStation(M, "ONE", {1, 1}),
   M2 = pollution:addValue(M1, "ONE", {{1, 1, 1}, {1, 1, 1}}, "PM10", 50),
   ?assertMatch({error, station_not_found}, pollution:addValue(M1, "TWO", calendar:local_time(), "PM10", 9) ),
+  ?assertMatch({error, incorrect_datatype}, pollution:addValue(M1, "ONE", {{1, 2, 3}}, "PM10", 9) ),
+  ?assertMatch({error, incorrect_datatype}, pollution:addValue(M1, "ONE", calendar:local_time(), 150, 9) ),
   ?assertMatch({error, measurement_exists}, pollution:addValue(M2, "ONE", {{1, 1, 1}, {1, 1, 1}}, "PM10", 9) ).
 
 removeValue_test()->
@@ -30,8 +34,9 @@ removeValue_test()->
   M2 = pollution:addValue(M1, "ONE", {{1, 1, 1}, {1, 1, 1}}, "PM10", 50),
   [H|_] = pollution:removeValue(M2, "ONE", {{1, 1, 1}, {1, 1, 1}}, "PM10"),
   {station, _, _, Ms} = H,
-  ?assertEqual(maps:size(Ms), 0),
-  pollution:removeValue(M2, "ONE", {{1, 1, 1}, {1, 1, 1}}, "PM10"). % quiet remove - nothing happens if not found
+  ?assertEqual(0, maps:size(Ms)),
+  M3 = pollution:removeValue(M2, "ONE", {{1, 1, 1}, {1, 1, 1}}, "PM10"), % quiet remove - nothing happens if not found
+  ?assertMatch([{station,{1,1},"ONE",#{}}], M3).
 
 getOneValue_test() ->
   M = pollution:createMonitor(),
@@ -46,8 +51,10 @@ getStationMean_test() ->
   M1 = pollution:addStation(M, "ONE", {1, 1}),
   M2 = pollution:addValue(M1, "ONE", {{1, 1, 1}, {1, 1, 1}}, "PM10", 50),
   M3 = pollution:addValue(M2, "ONE", {{1, 2, 1}, {1, 1, 1}}, "PM10", 40),
-  ?assertEqual(45.0, pollution:getStationMean(M3, "ONE", "PM10")),
-  ?assertEqual(0.0, pollution:getStationMean(M3, "ONE", "PM9")).
+  M4 = pollution:addStation(M3, "TWO", {2, 2}),
+  M5 = pollution:addValue(M4, "TWO", {{1, 2, 1}, {1, 1, 1}}, "PM10", 10),
+  ?assertEqual(45.0, pollution:getStationMean(M5, "ONE", "PM10")),
+  ?assertEqual(0.0, pollution:getStationMean(M5, "ONE", "PM9")).
 
 getDailyMean_test() ->
   M = pollution:createMonitor(),
@@ -63,8 +70,10 @@ getPeakHours_test() ->
   M1 = pollution:addStation(M, "ONE", {1, 1}),
   M2 = pollution:addStation(M1, "TWO", {2, 1}),
   M3 = pollution:addValue(M2, "ONE", {{1, 1, 1}, {18, 1, 1}}, "PM10", 50),
-  M4 = pollution:addValue(M3, "TWO", {{1, 1, 1}, {18, 1, 1}}, "PM10", 40),
-  {MaxVal, Hours} = pollution:getPeakHours(M4, "PM10"),
+  M4 = pollution:addValue(M3, "TWO", {{1, 1, 1}, {18, 20, 1}}, "PM10", 40),
+  M5 = pollution:addValue(M4, "TWO", {{1, 1, 1}, {19, 30, 1}}, "PM10", 40),
+  M6 = pollution:addValue(M5, "TWO", {{1, 1, 1}, {16, 50, 1}}, "PM10", 40),
+  {MaxVal, Hours} = pollution:getPeakHours(M6, "PM10"),
   ?assertEqual(MaxVal, 45.0),
   ?assertMatch(Hours, ["18"]).
 
